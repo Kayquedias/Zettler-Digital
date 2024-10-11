9a7d5c4808837d296a78170f214ec94c9727149a
Kayquedias <kayquedias204@gmail.com>
2024-10-10 23:56:09 -0300

    style: change responsiviness of cards slider section

--- js/slider.js
+++ js/slider.js
@@ -10,35 +10,6 @@ let currentIndex = 0;
 const totalCards = cards.length;
 const cardWidth = cards[0].offsetWidth + 24;
 
-function generateStars() {
-  for (let i = 0; i < totalCards; i++) {
-    const star = document.createElement('svg');
-    star.setAttribute('width', '14');
-    star.setAttribute('height', '15');
-    star.setAttribute('viewBox', '0 0 14 15');
-    star.setAttribute('fill', 'none');
-    star.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-    star.innerHTML = `
-      <path d="M7.0099 2.64339L14 0.583984L11.9604 7.59388L14 14.584L7.0099 12.5444L0 14.584L2.05941 7.59388L0 0.583984L7.0099 2.64339Z" fill="#191A23"/>
-    `;
-    slideStarContainer.appendChild(star);
-  }
-}
-
-function updateStars() {
-  const stars = slideStarContainer.querySelectorAll('svg');
-  stars.forEach((star, index) => {
-    if (index === currentIndex) {
-      star.querySelector('path').setAttribute('fill', '#0000FF');
-    } else {
-      star.querySelector('path').setAttribute('fill', '#191A23');
-    }
-  });
-}
-
-generateStars();
-updateStars();
-
 function updateSlidePosition() {
   cardContainer.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
   updateStars();
